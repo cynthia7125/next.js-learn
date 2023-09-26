8d753902d88db9c44c5ab1ff3c6b758a5486a796
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-09-26 14:12:00 +0300

    Styling in nextjs

--- app/conference/layout.js
+++ app/conference/layout.js
@@ -1,12 +1,7 @@
-import styles from "./styles.css"
-
 export default function ConferenceLayout({ children }) {
   return (
-      <>
-          <header style={styles.header}>
-            <h1>GLOBOMANTICS MANIACALLY TAKING TECH TO THE GLOBE</h1>
-          </header>
-          <section>{ children}</section>
-      </>
+    <>
+      <section>{children}</section>
+    </>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,42 @@
-import { Open_Sans } from '@next/font/google';
+import { Open_Sans } from "@next/font/google";
+import Link from "next/link";
+import Style from "./rootStyle.module.css";
+import "./global.css";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
-  weight: ["400"]
-})
+  weight: ["400"],
+});
 
-export default function RootLayout({
-  children,
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en" className={openSans.className}>
       <head></head>
-      <body>{children}</body>
+      <body>
+        <header>
+          <h2>
+            <Link className={Style.homeLink} href={"/home"}>
+              Globomantics
+            </Link>
+          </h2>
+          <h2>
+            <Link className={Style.menuBarLinks} href={"/blog"}>
+              Blog
+            </Link>
+          </h2>
+          <h2>
+            <Link className={Style.menuBarLinks} href={"/settings"}>
+              Settings
+            </Link>
+          </h2>
+          <h2>
+            <Link className={Style.menuBarLinks} href={"/conference"}>
+              Conference
+            </Link>
+          </h2>
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
